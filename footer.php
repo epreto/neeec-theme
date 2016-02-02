7d481ca7b4ee602b8286551fbd27be9eec0c6574
epreto <edmpreto@gmail.com>
2016-02-02 06:35:37 +0000

    add nav bootstrap files

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <ul class="list-inline text-center">
                     <li>
-                        <a href="http://twitter.com/neeec_aac">
+                        <a href="http://twitter.com/neeec_aac" target="_blank">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
@@ -12,7 +12,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="http://www.facebook.com/AAC.NEEEC/?fref=ts">
+                        <a href="http://www.facebook.com/AAC.NEEEC/?fref=ts" target="_blank">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
