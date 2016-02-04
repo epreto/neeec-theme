3be40be7e291f524507b766244ed49c34eaa189c
epreto <edmpreto@gmail.com>
2016-02-04 13:34:17 +0000

    add equipa page

--- page-contactos.php
+++ page-contactos.php
@@ -27,7 +27,7 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-            <p>Preenche então o formulário e rápidamente entraremos em contacto contigo!</p>
+            <p>Preenche então o formulário e rapidamente entraremos em contacto contigo!</p>
 
             <form name="sentMessage" id="contactForm" novalidate>
                 <div class="row control-group">
@@ -66,6 +66,14 @@
                     </div>
                 </div>
             </form>
+            <hr>
+            <!-- Map -->
+            <iframe width="100%" height="300px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3048.0164624849444!2d-8.418928384611112!3d40.1864470793925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd22f99534710f55%3A0x6f9fbebbec1bd8ea!2sDepartamento+de+Engenharia+Electrot%C3%A9cnica+e+de+Computadores!5e0!3m2!1spt-PT!2spt!4v1448860957689"></iframe>
+              <br/>
+              <small>
+                  <a href="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48755.789284588616!2d-8.399598431396484!3d40.20379451447572!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd22f99534710f55%3A0x6f9fbebbec1bd8ea!2sDepartamento+de+Engenharia+Electrot%C3%A9cnica+e+de+Computadores!5e0!3m2!1spt-PT!2spt!4v1448855198619"></a>
+              </small>
+            </iframe>
         </div>
     </div>
 </div>
